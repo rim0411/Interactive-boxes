b7b830c3919ec93015ea93fc375c8a43036076a9
RAMIZOU <rim.zouari@ensi-uma.tn>
2024-08-24 17:15:17 +0100

    local changes

--- src/app/components/option-selector/option-selector.component.ts
+++ src/app/components/option-selector/option-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { Ioption } from '../../models/option.model';
@@ -10,11 +10,10 @@ import { Ioption } from '../../models/option.model';
   styleUrl: './option-selector.component.css'
 })
 export class OptionSelectorComponent {
-  @Input() option: Ioption = { value: 0, color: '',idBox:0 };
+  @Input() option: Ioption = { value: 0, color: '', idBox: 0 };
   @Output() injectValueIntoBox = new EventEmitter<Ioption>();
 
-  
-// emitting an event to inject option selected into the selected box
+  // emitting an event to inject option selected into the selected box
   addValueToBox() {
     this.injectValueIntoBox.emit(this.option)
   }
--- src/app/signalsStore/options.store.ts
+++ src/app/signalsStore/options.store.ts
@@ -1,23 +1,17 @@
-import { HttpClient } from '@angular/common/http';
-import { inject } from '@angular/core';
+
 import {
   signalStore,
   withState,
   patchState,
   withMethods
 } from '@ngrx/signals';
-
-
-
-
 export const OptionsStore = signalStore(
   { providedIn: 'root' },
   withState({
     colorfulSelectedOption: -1
   }),
-
-  withMethods((store:any) => ({
-    setColorfulSelecctedOption(colorfulSelecctedOption:number) {
+  withMethods((store: any) => ({
+    setColorfulSelecctedOption(colorfulSelecctedOption: number) {
       patchState(store, { colorfulSelectedOption: colorfulSelecctedOption });
     }
   }))
